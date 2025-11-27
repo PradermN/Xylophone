493b41eb1170278dbd60e6c49b059d5e9993eb2b
PradermN <pradermnafack@gmail.com>
2025-11-27 23:49:01 +0100

    add new buttons for other sounds

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,119 @@ void main() => runApp(XylophoneApp());
 class XylophoneApp extends StatelessWidget {
   const XylophoneApp({super.key});
 
+  void playSound(int soundNumber) {
+    final player = AudioPlayer();
+    player.play(AssetSource('note$soundNumber.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final player = AudioPlayer();
-                player.play(AssetSource('note1.wav'));
-              },
-              child: Text(
-                'Play Sound',
-                style: TextStyle(fontSize: 24, color: Colors.black),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll <Color>(Colors.red),
+                  ),
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  child: Text(
+                    'Play Sound',
+                    style: TextStyle(fontSize: 24, color: Colors.black),
+                  ),
+                ),
               ),
-            ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll <Color>(Colors.orange),
+                  ),
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  child: Text(
+                    'Play Sound',
+                    style: TextStyle(fontSize: 24, color: Colors.black),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll <Color>(Colors.yellow),
+                  ),
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  child: Text(
+                    'Play Sound',
+                    style: TextStyle(fontSize: 24, color: Colors.black),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll <Color>(Colors.green),
+                  ),
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  child: Text(
+                    'Play Sound',
+                    style: TextStyle(fontSize: 24, color: Colors.black),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll <Color>(Colors.teal),
+                  ),
+                  onPressed: () {
+                   playSound(5);
+                  },
+                  child: Text(
+                    'Play Sound',
+                    style: TextStyle(fontSize: 24, color: Colors.black),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll <Color>(Colors.blue),
+                  ),
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  child: Text(
+                    'Play Sound',
+                    style: TextStyle(fontSize: 24, color: Colors.black),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll <Color>(Colors.purple),
+                  ),
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: Text(
+                    'Play Sound',
+                    style: TextStyle(fontSize: 24, color: Colors.black),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
